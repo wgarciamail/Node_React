522c914f63e45e866b52a1f874778fed8217da9a
William Garcia <wgarcia@tradercomusa.com>
2022-06-01 23:25:21 -0400

    Usa el paquete dotenv para definir en puerto en una variable de entorno.

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 
 const app = express()
@@ -7,6 +8,8 @@ app.get('/', (req, res)=> {
     res.status(200).send("<h1>Hola Mundo con NODEMON!</h1>")
 })
 
-app.listen(4000, () => {
-    console.log("Servidor escuchando en el puerto 4000")
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`)
 })
